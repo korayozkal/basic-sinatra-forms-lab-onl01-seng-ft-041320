bf9366e6c4388f76b1db17a77696764d741bf212
Koray Ozkal <koryozkal@gmail.com>
2020-06-25 04:46:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,7 +5,15 @@ class App < Sinatra::Base
     erb :newteam
   end   
 
+<<<<<<< HEAD
 post '/team' do
+=======
+get '/newteam' do
+    erb :newteam
+  end
+  
+post '/newteam' do
+>>>>>>> 6542be1306de5f4c4c2360a4a82c605d34daed81
     @team_name = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
@@ -13,8 +21,15 @@ post '/team' do
     @sf = params[:sf]
     @sg = params[:sg]
     @center = params[:c]
+<<<<<<< HEAD
 
     erb :team
   end
 
 end
+=======
+    erb :team
+  end
+end 
+  
+>>>>>>> 6542be1306de5f4c4c2360a4a82c605d34daed81
